d9cbfc516c9db561777f614db2515869810d493a
tortegaf <se.tortegaf@gmail.com>
2025-03-10 14:12:24 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/co/edu/javeriana/jpa_medievaltrader/MedievalTraderApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.jpa_medievaltrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalTraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/jpa_medievaltrader/MedievalTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.jpa_medievaltrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedievalTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
